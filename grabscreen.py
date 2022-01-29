5446e8b407a765b59a284a10350f981a232b5f77
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2022-01-29 22:06:26 +0800

    Create grabscreen.py

---
+++ grabscreen.py
@@ -0,0 +1,72 @@
+import win32gui
+import win32ui
+import win32con
+import win32api
+
+# 获取桌面
+hdesktop = win32gui.GetDesktopWindow()
+# 分辨率适应
+width = 500
+height = 500
+left = 100
+top = 100
+
+# 创建设备描述表
+desktop_dc = win32gui.GetWindowDC(hdesktop)
+img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+# 创建一个内存设备描述表
+mem_dc = img_dc.CreateCompatibleDC()
+# 创建位图对象
+screenshot = win32ui.CreateBitmap()
+screenshot.CreateCompatibleBitmap(img_dc, width, height)
+mem_dc.SelectObject(screenshot)
+# 截图至内存设备描述表
+mem_dc.BitBlt((0, 0), (width, height), img_dc, (0, 0), win32con.SRCCOPY)
+# 将截图保存到文件中
+screenshot.SaveBitmapFile(mem_dc, 'screenshot.jpg')
+# 内存释放
+mem_dc.DeleteDC()
+win32gui.DeleteObject(screenshot.GetHandle())
+
+#######################################################################
+
+# -*- coding: utf-8 -*-
+
+
+import cv2
+import numpy as np
+import win32gui, win32ui, win32con, win32api
+
+def grab_screen(region=None):
+
+    hwin = win32gui.GetDesktopWindow()
+
+    if region:
+            left,top,x2,y2 = region
+            width = x2 - left + 1
+            height = y2 - top + 1
+    else:
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    
+    signedIntsArray = bmp.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height,width,4)
+
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwin, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return img
+
